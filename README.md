ddbf5293929d825b81910c7ee6f26f390c4d6161
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2020-03-25 23:05:13 -0400

    Removed root module; add basic usage

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# emery
+# Emery
+
+Emery is a type safety library for Ruby. It provides a way to define types and serialize/deserialize them to/from JSON with type certainty and safety
+
+## Basic Usage Example
+
+Here's basic example of Emery type checking:
+```ruby
+require 'emery'
+
+my_var = T.check(T.array(String), ["the string"])
+# my_var is ["the string"]
+
+my_var = T.check(T.array(String), "the string")
+# Throws: Value '"the string"' type is String - Array is required
+```
+
+Here's type safe JSON serialization/deserialization:
+```ruby
+require 'emery'
+
+the_json = Jsoner.to_json(T.array(DateTime), [DateTime.new(2019, 11, 30, 17, 45, 55)])
+# the_json is '["2019-11-30T17:45:55"]'
+
+data = Jsoner.from_json(T.array(DateTime), '["2019-11-30T17:45:55+00:00"]')
+# data is Array with the only one item which is corresponding DateTime
+```
--- test/enum_test.rb
+++ test/enum_test.rb
@@ -1,47 +1,43 @@
 require "test/unit/runner/junitxml"
 
-require 'emery/type'
-require 'emery/enum'
-require 'emery/jsoner'
+require 'emery'
 
-module Emery
-  class TypeCheckEnum < Test::Unit::TestCase
-    def test_success
-      assert_equal SomeEnum::one, T.check(SomeEnum, SomeEnum::one), "Plain enum type should pass type check"
-    end
-
-    def test_fail
-      assert_raise TypeError do
-        T.check(SomeEnum, "non existing")
-      end
-    end
+class TypeCheckEnum < Test::Unit::TestCase
+  def test_success
+    assert_equal SomeEnum::one, T.check(SomeEnum, SomeEnum::one), "Plain enum type should pass type check"
   end
 
-  class EnumDeserialization < Test::Unit::TestCase
-    def test_deserialize_enum
-      data = Jsoner.from_json(SomeEnum, '"two"')
-      T.check(SomeEnum, data)
-      assert_equal SomeEnum::two, data, "Enum should be parsable from JSON"
-    end
-
-    def test_deserialize_enum_non_existing_item
-      assert_raise JsonerError do
-        Jsoner.from_json(SomeEnum, '"non_existing"')
-      end
+  def test_fail
+    assert_raise TypeError do
+      T.check(SomeEnum, "non existing")
     end
   end
+end
 
-  class EnumSerialization < Test::Unit::TestCase
-    def test_serialize_enum
-      json_str = Jsoner.to_json(SomeEnum, SomeEnum::two)
-      assert_equal '"two"', json_str, "Enum should be serializable to JSON"
+class EnumDeserialization < Test::Unit::TestCase
+  def test_deserialize_enum
+    data = Jsoner.from_json(SomeEnum, '"two"')
+    T.check(SomeEnum, data)
+    assert_equal SomeEnum::two, data, "Enum should be parsable from JSON"
+  end
+
+  def test_deserialize_enum_non_existing_item
+    assert_raise JsonerError do
+      Jsoner.from_json(SomeEnum, '"non_existing"')
     end
   end
+end
 
-  class SomeEnum
-    include Enum
-
-    define :one, 'one'
-    define :two, 'two'
+class EnumSerialization < Test::Unit::TestCase
+  def test_serialize_enum
+    json_str = Jsoner.to_json(SomeEnum, SomeEnum::two)
+    assert_equal '"two"', json_str, "Enum should be serializable to JSON"
   end
+end
+
+class SomeEnum
+  include Enum
+
+  define :one, 'one'
+  define :two, 'two'
 end
